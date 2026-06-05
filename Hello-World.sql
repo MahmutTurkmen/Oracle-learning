9227eba333c330e65126689332297f7cda6e049a
MahmutTurkmen <150715109+MahmutTurkmen@users.noreply.github.com>
2026-06-05 22:45:45 +0300

    Create Hello-World.sql

---
+++ Hello-World.sql
@@ -0,0 +1,9 @@
+-- Oracle SQL Merhaba Dünya örneği
+select 'Merhaba Dünya' as Hello_World from dual; 
+
+-- DUAL: Oracle'a özgü, tek satır ve tek sütundan oluşan sanal bir sistem tablosudur.
+-- DUAL: Herhangi bir fiziksel tabloya ihtiyaç duymadan fonksiyonları veya metinleri test etmek için kullanılır. 
+-- select komutu bir dml(Data Manipulation Language) komutudur. Veritabanındaki verileri listelemek için kullanılır.
+-- dml(Data Manipulation Language): Veritabanındaki mevcut verileri sorgulamak(select), veritabanına yeni veri eklemek(insert), 
+-- veritabanındaki verileri değiştirmek(update), veritabanından veri silmek(delete) için kullanılan 
+-- SQL(Structured Query Language/Yapılandırılmış Sorgu Dili) komutlarının tamamıdır.
